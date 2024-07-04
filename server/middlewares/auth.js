30d40aafbfd70a623327a343dd4ced011d2821d0
Awais Tariq <awais@terminustech.com>
2024-07-04 17:54:55 +0500

    extra console removed

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -6,7 +6,6 @@ function authenticateToken(req, res, next) {
   if (token == null) return res.status(401).send('Access denied. No token provided.');
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    console.log(err,user);
     // if (err) return res.sendStatus(403).send('Invalid token.');
     if (err) return res.status(403).send('Invalid token.');
     req.user = user;
